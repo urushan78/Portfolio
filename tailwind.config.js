84c3da4078897ad7b1dd237d8732d0f77f866d8a
urushan78 <urushaniroula@gmail.com>
2025-06-22 12:57:25 +0545

    Add .gitignore and update output.css and tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: "class", 
   content: ["./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 1s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
   },
   plugins: [],
 };
